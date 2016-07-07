f432447a0902c8667bee7fd8a485099b9a702765
Mateusz Iwaniszczuk <MrVanosh@MacBook-Pro-Mateusz.local>
2016-07-07 14:03:09 +0200

    Added player class

---
+++ core/src/pl/clicker/game/entities/Player.java
@@ -0,0 +1,23 @@
+package pl.clicker.game.entities;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Player extends Image {
+
+	private final static int WIDTH = 77;
+	private final static int HEIGHT = 152;
+	
+	private final static int STARTINX_X = 200;
+	private final static int STARTING_Y = 300;
+	
+	public Player(){
+		super(new Texture("badlogic.jpg"));
+		
+		this.setOrigin(WIDTH / 2, HEIGHT / 2);
+		this.setSize(WIDTH, HEIGHT);
+		
+		//starting position
+		this.setPosition(STARTINX_X, STARTING_Y);
+	}
+	
+}
