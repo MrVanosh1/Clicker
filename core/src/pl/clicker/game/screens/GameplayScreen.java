9b75c04b266b6422ff6f321f634b3dfd8f994f89
Mateusz Iwaniszczuk <MrVanosh@MacBook-Pro-Mateusz.local>
2016-07-07 14:11:29 +0200

    Displaying Player Image

--- core/src/pl/clicker/game/screens/GameplayScreen.java
+++ core/src/pl/clicker/game/screens/GameplayScreen.java
@@ -1,16 +1,39 @@
 package pl.clicker.game.screens;
 
 import pl.clicker.game.ClickerGame;
+import pl.clicker.game.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
 
+	private Player player;
+	
 	public GameplayScreen(ClickerGame game) {
 		super(game);
 		init();
 	}
 
 	private void init() {
+		initPlayer();
+	}
+
+	private void initPlayer() {
+		player = new Player();
+		stage.addActor(player);
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		super.render(delta);
+		update();
 		
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 	}
 
 }
