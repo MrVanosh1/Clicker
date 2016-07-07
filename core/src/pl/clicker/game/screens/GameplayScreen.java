8e445b2ad7ab4731f1240e2fc7fac3d667980117
Mateusz Iwaniszczuk <MrVanosh@MacBook-Pro-Mateusz.local>
2016-07-07 14:53:50 +0200

    added some nice player click effect

--- core/src/pl/clicker/game/screens/GameplayScreen.java
+++ core/src/pl/clicker/game/screens/GameplayScreen.java
@@ -34,6 +34,8 @@ public class GameplayScreen extends AbstractScreen {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
 				
+				player.reactOnClick();
+				
 				return super.touchDown(event, x, y, pointer, button);
 			}
 			
