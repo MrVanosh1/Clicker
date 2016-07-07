359a81e528a9a7de03df6c3d82301c0224b03e5e
Mateusz Iwaniszczuk <MrVanosh@MacBook-Pro-Mateusz.local>
2016-07-07 13:51:37 +0200

    Added dummy GameplayScreen

---
+++ core/src/pl/clicker/game/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package pl.clicker.game.screens;
+
+import pl.clicker.game.ClickerGame;
+
+public class GameplayScreen extends AbstractScreen {
+
+	public GameplayScreen(ClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+		
+	}
+
+}
